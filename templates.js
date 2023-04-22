44b2c7b851c12d77a0f70c7f733f2db5f0f656f1
Ryan Logan <ryan.logan@navexglobal.com>
2023-04-21 18:44:55 -0700

    added template variables to email notification template

--- templates.js
+++ templates.js
@@ -1009,9 +1009,9 @@ var dataTableTwo = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "ht
                             <table border="0" cellpadding="30" cellspacing="0" width="100%">
                               <tr>
                                 <td align="center" valign="top" class="textContent">
-                                  <h1 style="color:#FFFFFF;line-height:100%;font-family:Helvetica,Arial,sans-serif;font-size:35px;font-weight:normal;margin-bottom:5px;text-align:center;">{{organizationName}} New Assignment</h1>
-                                  <h2 style="text-align:center;font-weight:normal;font-family:Helvetica,Arial,sans-serif;font-size:23px;margin-bottom:10px;color:#C9BC20;line-height:135%;">{{projectName}}</h2>
-                                  <div style="text-align:center;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;color:#FFFFFF;line-height:135%;">You have been assigned a {{taskType}} </div>
+                                  <h1 style="color:#FFFFFF;line-height:100%;font-family:Helvetica,Arial,sans-serif;font-size:35px;font-weight:normal;margin-bottom:5px;text-align:center;">*Name of Organization*</h1>
+                                  <h2 style="text-align:center;font-weight:normal;font-family:Helvetica,Arial,sans-serif;font-size:23px;margin-bottom:10px;color:#C9BC20;line-height:135%;">[VendorName]</h2>
+                                  <div style="text-align:center;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;color:#FFFFFF;line-height:135%;">Hello [VendorContact], You have been assigned the following assessment: [AssessmentName] to complete on behalf of your organization. </div>
                                 </td>
                               </tr>
                             </table>
@@ -1038,8 +1038,8 @@ var dataTableTwo = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "ht
                                   <table border="0" cellpadding="0" cellspacing="0" width="100%">
                                     <tr>
                                       <td valign="top" class="textContent">
-                                        <h3 style="color:#5F5F5F;line-height:125%;font-family:Helvetica,Arial,sans-serif;font-size:20px;font-weight:normal;margin-top:0;margin-bottom:3px;text-align:left;">{{assignmentName}}</h3>
-                                        <div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">A new {{taskType}} has been assigned to you for the {{projectName}} project.{{#if dueDate }}Please ensure the assigned task is completed by {{dueDate}} {{/if}}</div>
+                                        <h3 style="color:#5F5F5F;line-height:125%;font-family:Helvetica,Arial,sans-serif;font-size:20px;font-weight:normal;margin-top:0;margin-bottom:3px;text-align:left;">Assessment Information</h3>
+                                        <div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:15px;margin-bottom:0;margin-top:3px;color:#5F5F5F;line-height:135%;">This assessment is designed to help us meet and maintain compliance to industry regulations and security expectations. Click the link below to access and complete the assessment. Please do so by [DueDate]. If you have any questions, feel free to reach out to *person@org.com*</div>
                                       </td>
                                     </tr>
                                   </table>
@@ -1070,7 +1070,7 @@ var dataTableTwo = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "ht
                                   <table border="0" cellpadding="0" cellspacing="0" width="50%" class="emailButton" style="background-color: #2E7D32;">
                                     <tr>
                                       <td align="center" valign="middle" class="buttonContent" style="padding-top:15px;padding-bottom:15px;padding-right:15px;padding-left:15px;">
-                                        <a style="color:#FFFFFF;text-decoration:none;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:135%;" href="{{link}}" target="_blank">{{linkText}}</a>
+                                        <span style="color:#FFFFFF;text-decoration:none;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:135%; cursor: pointer; target="_blank">Click here to complete [AssessmentUrl]</span>
                                       </td>
                                     </tr>
                                   </table>
